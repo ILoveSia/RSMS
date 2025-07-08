668fc3325dd8688b1606519d739e616d6a848755
oot R <dolphine0019@gmail.com>
2025-07-08 09:37:25 +0900

    상세 페이지 컴포넌트화

--- frontend/src/shared/components/modal/BaseDialog.tsx
+++ frontend/src/shared/components/modal/BaseDialog.tsx
@@ -18,7 +18,6 @@ export interface BaseDialogProps {
   mode: DialogMode;
   title: string;
   maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-  fullWidth?: boolean;
   children: ReactNode;
   onClose: () => void;
   onSave?: () => void;
