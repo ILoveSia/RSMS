653c2c59a1641931cd686f4173ea89774926e83a
samidare20 <dolphine0019@gmail.com>
2025-07-14 15:41:26 +0900

    회의 카드 및 포지션 카드 컴포넌트 삭제, 관련된 인덱스 파일에서 내보내기 제거

--- frontend/src/domains/ledgermngt/components/index.ts
+++ frontend/src/domains/ledgermngt/components/index.ts
@@ -1,6 +1,4 @@
 export { default as MeetingBodyDialog } from './MeetingBodyDialog';
-export { default as MeetingCard } from './MeetingCard';
-export { default as PositionCard } from './PositionCard';
 export { default as PositionDialog } from './PositionDialog';
 export { default as ResponsibilityCard } from './ResponsibilityCard';
 export { default as ResponsibilityDialog } from './ResponsibilityDialog';
