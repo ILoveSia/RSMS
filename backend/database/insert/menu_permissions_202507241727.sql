18a704036f4e63f042229cd126e887fb8bb22829
Your Name <you@example.com>
2025-07-24 17:30:15 +0900

    SQL문01

---
+++ backend/database/insert/menu_permissions_202507241727.sql
@@ -0,0 +1,44 @@
+INSERT INTO public.menu_permissions (menu_id,role_name,can_read,can_write,can_delete,created_at,updated_at,created_id,updated_id) VALUES
+	 (55,'ADMIN',true,true,true,'2025-07-19 22:56:32.587933+09','2025-07-19 22:56:32.587933+09','system','system'),
+	 (55,'USER',true,false,false,'2025-07-19 22:56:32.596012+09','2025-07-19 22:56:32.596012+09','system','system'),
+	 (56,'ADMIN',true,true,true,'2025-07-19 22:56:32.643851+09','2025-07-19 22:56:32.643851+09','system','system'),
+	 (56,'USER',true,false,false,'2025-07-19 22:56:32.654247+09','2025-07-19 22:56:32.654247+09','system','system'),
+	 (57,'ADMIN',true,true,true,'2025-07-19 22:56:32.691961+09','2025-07-19 22:56:32.691961+09','system','system'),
+	 (57,'USER',true,false,false,'2025-07-19 22:56:32.704017+09','2025-07-19 22:56:32.704017+09','system','system'),
+	 (58,'ADMIN',true,true,true,'2025-07-19 22:56:32.719888+09','2025-07-19 22:56:32.719888+09','system','system'),
+	 (58,'USER',true,false,false,'2025-07-19 22:56:32.73554+09','2025-07-19 22:56:32.73554+09','system','system'),
+	 (59,'ADMIN',true,true,true,'2025-07-19 22:56:32.755397+09','2025-07-19 22:56:32.755397+09','system','system'),
+	 (59,'USER',true,false,false,'2025-07-19 22:56:32.770509+09','2025-07-19 22:56:32.770509+09','system','system');
+INSERT INTO public.menu_permissions (menu_id,role_name,can_read,can_write,can_delete,created_at,updated_at,created_id,updated_id) VALUES
+	 (60,'ADMIN',true,true,true,'2025-07-19 22:56:32.806619+09','2025-07-19 22:56:32.806619+09','system','system'),
+	 (60,'USER',true,false,false,'2025-07-19 22:56:32.820934+09','2025-07-19 22:56:32.820934+09','system','system'),
+	 (61,'ADMIN',true,true,true,'2025-07-19 22:56:32.849132+09','2025-07-19 22:56:32.849132+09','system','system'),
+	 (15,'ADMIN',true,true,true,'2025-06-27 17:32:06.328283+09','2025-06-27 17:32:06.328283+09',NULL,NULL),
+	 (61,'USER',true,false,false,'2025-07-19 22:56:32.866766+09','2025-07-19 22:56:32.866766+09','system','system'),
+	 (62,'ADMIN',true,true,true,'2025-07-19 22:56:32.936509+09','2025-07-19 22:56:32.936509+09','system','system'),
+	 (62,'USER',true,false,false,'2025-07-19 22:56:32.953478+09','2025-07-19 22:56:32.953478+09','system','system'),
+	 (63,'ADMIN',true,true,true,'2025-07-19 22:56:32.988751+09','2025-07-19 22:56:32.988751+09','system','system'),
+	 (63,'USER',true,false,false,'2025-07-19 22:56:33.007059+09','2025-07-19 22:56:33.007059+09','system','system'),
+	 (64,'ADMIN',true,true,true,'2025-07-19 22:56:33.03678+09','2025-07-19 22:56:33.03678+09','system','system');
+INSERT INTO public.menu_permissions (menu_id,role_name,can_read,can_write,can_delete,created_at,updated_at,created_id,updated_id) VALUES
+	 (64,'USER',true,false,false,'2025-07-19 22:56:33.054834+09','2025-07-19 22:56:33.054834+09','system','system'),
+	 (65,'ADMIN',true,true,true,'2025-07-19 22:56:33.091124+09','2025-07-19 22:56:33.091124+09','system','system'),
+	 (65,'USER',true,false,false,'2025-07-19 22:56:33.104155+09','2025-07-19 22:56:33.104155+09','system','system'),
+	 (66,'ADMIN',true,true,true,'2025-07-19 22:56:33.124554+09','2025-07-19 22:56:33.124554+09','system','system'),
+	 (66,'USER',true,false,false,'2025-07-19 22:56:33.132024+09','2025-07-19 22:56:33.132024+09','system','system'),
+	 (67,'ADMIN',true,true,true,'2025-07-19 22:56:33.163042+09','2025-07-19 22:56:33.163042+09','system','system'),
+	 (67,'USER',true,false,false,'2025-07-19 22:56:33.178601+09','2025-07-19 22:56:33.178601+09','system','system'),
+	 (15,'USER',true,false,false,'2025-06-27 17:32:12.166411+09','2025-06-27 17:32:12.166411+09',NULL,NULL),
+	 (68,'ADMIN',true,true,true,'2025-07-19 22:56:33.208058+09','2025-07-19 22:56:33.208058+09','system','system'),
+	 (68,'USER',true,false,false,'2025-07-19 22:56:33.233561+09','2025-07-19 22:56:33.233561+09','system','system');
+INSERT INTO public.menu_permissions (menu_id,role_name,can_read,can_write,can_delete,created_at,updated_at,created_id,updated_id) VALUES
+	 (69,'ADMIN',true,true,true,'2025-07-19 22:56:33.282549+09','2025-07-19 22:56:33.282549+09','system','system'),
+	 (69,'USER',true,false,false,'2025-07-19 22:56:33.290455+09','2025-07-19 22:56:33.290455+09','system','system'),
+	 (70,'ADMIN',true,true,true,'2025-07-19 22:56:33.33712+09','2025-07-19 22:56:33.33712+09','system','system'),
+	 (70,'USER',true,false,false,'2025-07-19 22:56:33.355677+09','2025-07-19 22:56:33.355677+09','system','system'),
+	 (71,'ADMIN',true,true,true,'2025-07-19 22:56:33.376539+09','2025-07-19 22:56:33.376539+09','system','system'),
+	 (71,'USER',true,false,false,'2025-07-19 22:56:33.40135+09','2025-07-19 22:56:33.40135+09','system','system'),
+	 (72,'ADMIN',true,true,true,'2025-07-19 22:56:33.429773+09','2025-07-19 22:56:33.429773+09','system','system'),
+	 (72,'USER',true,false,false,'2025-07-19 22:56:33.43554+09','2025-07-19 22:56:33.43554+09','system','system'),
+	 (73,'ADMIN',true,true,true,'2025-07-19 22:56:33.462051+09','2025-07-19 22:56:33.462051+09','system','system'),
+	 (73,'USER',true,false,false,'2025-07-19 22:56:33.474567+09','2025-07-19 22:56:33.474567+09','system','system');
