2a8e25754d5eeaa33d80a7aef9286ee34fe14e8d
Your Name <you@example.com>
2025-08-22 09:37:14 +0900

    공통코드 inset

---
+++ backend/database/insert/common_code_202508220935.sql
@@ -0,0 +1,123 @@
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('PRD03','PERIOD','분기','2025-06-30 16:24:53.884','SYSTEM','분기',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PRD04','PERIOD','월','2025-06-30 16:24:53.884','SYSTEM','월',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CRT05','COM_ROLE_TYPE','내부통제 공통(리스크)','2025-06-30 16:24:53.884','SYSTEM','부서공통항목 내부통제 공통(리스크)',5,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CRT06','COM_ROLE_TYPE','내부통제 공통(재무)','2025-06-30 16:24:53.884','SYSTEM','부서공통항목 내부통제 공통(재무)',5,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('R001','JOB_RANK','사원','2025-06-30 16:20:25.03243','SYSTEM','일반 사원',1,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('R002','JOB_RANK','대리','2025-06-30 16:20:25.03243','SYSTEM','대리급',2,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('R003','JOB_RANK','과장','2025-06-30 16:20:25.03243','SYSTEM','과장급',3,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('R004','JOB_RANK','차장','2025-06-30 16:20:25.03243','SYSTEM','차장급',4,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('R005','JOB_RANK','부장','2025-06-30 16:20:25.03243','SYSTEM','부장급',5,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('R007','JOB_RANK','상무','2025-06-30 16:20:25.03243','SYSTEM','상무이사',7,'2025-06-30 16:20:25.03243','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('R008','JOB_RANK','전무','2025-06-30 16:20:25.03243','SYSTEM','전무이사',8,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('R009','JOB_RANK','부사장','2025-06-30 16:20:25.03243','SYSTEM','부사장',9,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('R010','JOB_RANK','사장','2025-06-30 16:20:25.03243','SYSTEM','사장',10,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('GB01','MEETING_BODY','등록','2025-06-30 16:24:53.884','SYSTEM','등록',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('GB02','MEETING_BODY','삭제','2025-06-30 16:24:53.884','SYSTEM','삭제',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PRD01','PERIOD','년','2025-06-30 16:24:53.884','SYSTEM','년도',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PRD02','PERIOD','반기','2025-06-30 16:24:53.884','SYSTEM','반기',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PRD05','PERIOD','수시','2025-06-30 16:24:53.884','SYSTEM','수시',5,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MG01','MEB_GUBUN','위원장','2025-06-30 16:24:53.884','SYSTEM','위원장',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MG02','MEB_GUBUN','위원','2025-06-30 16:24:53.884','SYSTEM','위원',2,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('CFM01','CONFIRM_GUBUN','확정','2025-06-30 16:24:53.884','SYSTEM','확정',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CFM02','CONFIRM_GUBUN','취소','2025-06-30 16:24:53.884','SYSTEM','취소',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('P2','ORDER_STATUS','직책확정','2025-06-30 16:24:53.884','SYSTEM','직책확정',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('P3','ORDER_STATUS','직책별책무확정','2025-06-30 16:24:53.884','SYSTEM','직책별책무확정',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('P1','ORDER_STATUS','신규','2025-06-30 16:24:53.884','SYSTEM','신규',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CRT','FIELD_TYPE','부서공통항목','2025-06-30 16:24:53.884','SYSTEM','항목구분코드',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('URT','FIELD_TYPE','부서고유항목','2025-06-30 16:24:53.884','SYSTEM','항목구분코드',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('URT01','UNI_ROLE_TYPE','부서고유','2025-06-30 16:24:53.884','SYSTEM','부서고유항목 부서고유',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('URT02','UNI_ROLE_TYPE','부서별 (공통)','2025-06-30 16:24:53.884','SYSTEM','부서고유항목 부서별 (공통)',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CRT01','COM_ROLE_TYPE','내부통제 공통(준법)','2025-06-30 16:24:53.884','SYSTEM','부서공통항목 내부통제 공통(준법)',1,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('CRT02','COM_ROLE_TYPE','내부통제 공통(HR)','2025-06-30 16:24:53.884','SYSTEM','부서공통항목 내부통제 공통(HR)',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CRT03','COM_ROLE_TYPE','내부통제 공통(감사)','2025-06-30 16:24:53.884','SYSTEM','부서공통항목 내부통제 공통(감사)',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CRT04','COM_ROLE_TYPE','내부통제 공통(공시)','2025-06-30 16:24:53.884','SYSTEM','부서공통항목 내부통제 공통(공시)',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON01','MONTH','1월','2025-06-30 16:24:53.884','SYSTEM','점검시기1월',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON02','MONTH','2월','2025-06-30 16:24:53.884','SYSTEM','점검시기2월',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON03','MONTH','3월','2025-06-30 16:24:53.884','SYSTEM','점검시기3월',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON04','MONTH','4월','2025-06-30 16:24:53.884','SYSTEM','점검시기4월',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON05','MONTH','5월','2025-06-30 16:24:53.884','SYSTEM','점검시기5월',5,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON06','MONTH','6월','2025-06-30 16:24:53.884','SYSTEM','점검시기6월',6,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON07','MONTH','7월','2025-06-30 16:24:53.884','SYSTEM','점검시기7월',7,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('MON08','MONTH','8월','2025-06-30 16:24:53.884','SYSTEM','점검시기8월',8,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON09','MONTH','9월','2025-06-30 16:24:53.884','SYSTEM','점검시기9월',9,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON10','MONTH','10월','2025-06-30 16:24:53.884','SYSTEM','점검시기10월',10,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON11','MONTH','11월','2025-06-30 16:24:53.884','SYSTEM','점검시기11월',11,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('MON12','MONTH','12월','2025-06-30 16:24:53.884','SYSTEM','점검시기12월',12,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('AUDIT001','TASK_TYPE','점검 계획','2025-06-30 16:24:53.884','SYSTEM','점검 계획',9,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('AUDIT002','TASK_TYPE','점검 현황(항목별)','2025-06-30 16:24:53.884','SYSTEM','점검 현황(항목별)',10,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('AUDIT003','TASK_TYPE','점검 현황(부서별)','2025-06-30 16:24:53.884','SYSTEM','점검 현황(부서별)',11,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('AUDIT004','TASK_TYPE','미흡사항 현황','2025-06-30 16:24:53.884','SYSTEM','미흡사항 현황',12,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('SUBMITTED','APPR_STAT_CD','상신','2025-06-30 16:24:53.884','SYSTEM','상신',1,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('IN_PROGRESS','APPR_STAT_CD','진행중','2025-06-30 16:24:53.884','SYSTEM','진행중',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('REJECTED','APPR_STAT_CD','반려','2025-06-30 16:24:53.884','SYSTEM','반려',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('P4','ORDER_STATUS','임원확정','2025-06-30 16:24:53.884','SYSTEM','임원확정',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('role_resp_status','TASK_TYPE','직책별 책무 ','2025-06-30 16:24:53.884','SYSTEM','직책별 책무',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('R006','JOB_RANK','이사','2025-06-30 16:20:25.03243','SYSTEM','이사급',6,'2025-06-30 16:20:25.03243','SYSTEM','Y'),
+	 ('INS01','INSPEC_RESULT','진행중','2025-06-30 16:24:53.884','SYSTEM','진행중',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('INS02','INSPEC_RESULT','적정','2025-06-30 16:24:53.884','SYSTEM','적정',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('INS03','INSPEC_RESULT','미흡','2025-06-30 16:24:53.884','SYSTEM','미흡',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('INS04','INSPEC_RESULT','제외','2025-06-30 16:24:53.884','SYSTEM','제외',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PENDING','QNA_STATUS','답변대기','2025-06-30 16:24:53.884','SYSTEM','답변대기',1,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('ANSWERED','QNA_STATUS','답변완료','2025-06-30 16:24:53.884','SYSTEM','답변완료',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CLOSED','QNA_STATUS','종료','2025-06-30 16:24:53.884','SYSTEM','종료',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('AA01','AUDIT_APPLY','점검신청','2025-06-30 16:24:53.884','SYSTEM','점검신청',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('AA03','AUDIT_APPLY','점검마감','2025-06-30 16:24:53.884','SYSTEM','점검마감',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('AA02','AUDIT_APPLY','점검진행','2025-06-30 16:24:53.884','SYSTEM','점검진행',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('APPROVED','APPR_STAT_CD','승인','2025-06-30 16:24:53.884','SYSTEM','승인',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PENDING','STEP_STATUS','진행중','2025-06-30 16:24:53.884','SYSTEM','진행중',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('WAITING','STEP_STATUS','순서대기','2025-06-30 16:24:53.884','SYSTEM','순서대기',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('APPROVED','STEP_STATUS','승인','2025-06-30 16:24:53.884','SYSTEM','승인',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('REJECTED','STEP_STATUS','반려','2025-06-30 16:24:53.884','SYSTEM','반려',4,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('P5','ORDER_STATUS','최종확정','2025-06-30 16:24:53.884','SYSTEM','최종확정',5,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('P6','ORDER_HOD_STATUS','진행중','2025-06-30 16:24:53.884','SYSTEM','진행중',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('hod_ic_item','TASK_TYPE','부장 내부통제 항목','2025-06-30 16:24:53.884','SYSTEM','부장 내부통제 항목',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('NORMAL','URGENCY','일반','2025-06-30 16:24:53.884','SYSTEM','일반',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('URGENT','URGENCY','긴급','2025-06-30 16:24:53.884','SYSTEM','긴급',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('POSITION','HANDOVER_TYPE','직책','2025-06-30 16:24:53.884','SYSTEM','직책',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('RESPONSIBILITY','HANDOVER_TYPE','직무','2025-06-30 16:24:53.884','SYSTEM','직무',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PLANNED','BUSINESSPLAN_STATUS','계획됨','2025-06-30 16:24:53.884','SYSTEM','계획됨',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('IN_PROGRESS','BUSINESSPLAN_STATUS','진행중','2025-06-30 16:24:53.884','SYSTEM','진행중',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('COMPLETED','BUSINESSPLAN_STATUS','완료','2025-06-30 16:24:53.884','SYSTEM','완료',3,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('CANCELLED','BUSINESSPLAN_STATUS','취소됨','2025-06-30 16:24:53.884','SYSTEM','취소됨',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('QUARTERLY','BUSINESSPLAN_STATUS','분기별','2025-06-30 16:24:53.884','SYSTEM','분기별',5,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('ANNUAL','BUSINESSPLAN_STATUS','연간','2025-06-30 16:24:53.884','SYSTEM','연간',6,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('SPECIAL','BUSINESSPLAN_STATUS','특별점검','2025-06-30 16:24:53.884','SYSTEM','특별점검',7,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PLANNED','HANDOVER_STATUS','계획됨','2025-06-30 16:24:53.884','SYSTEM','계획됨',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('IN_PROGRESS','HANDOVER_STATUS','진행중','2025-06-30 16:24:53.884','SYSTEM','진행중',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('COMPLETED','HANDOVER_STATUS','완료','2025-06-30 16:24:53.884','SYSTEM','완료',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CANCELLED','HANDOVER_STATUS','취소됨','2025-06-30 16:24:53.884','SYSTEM','취소됨',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('QUARTERLY','INSPECTION_TYPE','분기별','2025-06-30 16:24:53.884','SYSTEM','분기별',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('SEMI_ANNUAL','INSPECTION_TYPE','반기별','2025-06-30 16:24:53.884','SYSTEM','반기별',2,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('ANNUAL','INSPECTION_TYPE','연간','2025-06-30 16:24:53.884','SYSTEM','연간',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('SPECIAL','INSPECTION_TYPE','특별','2025-06-30 16:24:53.884','SYSTEM','특별',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('responsibility_documents','TASK_TYPE','책무기술서 관리','2025-06-30 16:24:53.884','SYSTEM','책무기술서 관리',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('DRAFT','RESPONSIBILITY_STATUS','초안','2025-06-30 16:24:53.884','SYSTEM','초안',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('REVIEW','RESPONSIBILITY_STATUS','검토중','2025-06-30 16:24:53.884','SYSTEM','검토중',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('APPROVED','RESPONSIBILITY_STATUS','승인완료','2025-06-30 16:24:53.884','SYSTEM','승인완료',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PUBLISHED','RESPONSIBILITY_STATUS','발행완료','2025-06-30 16:24:53.884','SYSTEM','발행완료',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('P7','ORDER_HOD_STATUS','부서장확정','2025-06-30 16:24:53.884','SYSTEM','부서장확정',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('P8','ORDER_HOD_STATUS','부서장최종확정','2025-06-30 16:24:53.884','SYSTEM','부서장최종확정',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('internal_control_manuals','TASK_TYPE','내부통제 업무메뉴얼 관리','2025-06-30 16:24:53.884','SYSTEM','내부통제 업무메뉴얼 관리',4,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('B01','BANK_CD','금융감독원','2025-06-30 16:24:53.884','SYSTEM','금융감독원',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('B02','BANK_CD','증권거래소','2025-06-30 16:24:53.884','SYSTEM','증권거래소',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PLI01','PLAN_IMP','개선계획작성','2025-06-30 16:24:53.884','SYSTEM','개선계획작성',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PLI02','PLAN_IMP','이행결과작성','2025-06-30 16:24:53.884','SYSTEM','이행결과작성',2,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('PLI03','PLAN_IMP','이행결과결재 완료','2025-06-30 16:24:53.884','SYSTEM','이행결과결재 완료',3,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('audit_prog_mngt_detail','TASK_TYPE','미흡사항(이행결과작성)','2025-06-30 16:24:53.884','SYSTEM','미흡사항(이행결과작성)',5,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('audit_result_report','TASK_TYPE','점검결과보고','2025-06-30 16:24:53.884','SYSTEM','점검결과보고',6,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CAT004','CATEGORY','점검','2025-06-30 16:24:53.884','SYSTEM','점검',4,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CAT001','CATEGORY','시스템','2025-06-30 16:24:53.884','SYSTEM','시스템',1,'2025-06-30 16:24:53.884','SYSTEM','Y'),
+	 ('CAT002','CATEGORY','권한','2025-06-30 16:24:53.884','SYSTEM','권한',2,'2025-06-30 16:24:53.884','SYSTEM','Y');
+INSERT INTO public.common_code (code,group_code,code_name,created_at,created_id,description,sort_order,updated_at,updated_id,use_yn) VALUES
+	 ('CAT003','CATEGORY','업무','2025-06-30 16:24:53.884','SYSTEM','업무',3,'2025-06-30 16:24:53.884','SYSTEM','Y');
